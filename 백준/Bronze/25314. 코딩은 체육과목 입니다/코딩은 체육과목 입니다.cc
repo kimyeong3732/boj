adb32506fd9133ae92227c08a6d969b22c94c349
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 17:28:25 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,13 @@
+#include <cstdio>
+
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+    
+    for(int i=0; i<(n/4); i++)
+        printf("long ");
+    printf("int\n");
+    
+    return 0;
+}
