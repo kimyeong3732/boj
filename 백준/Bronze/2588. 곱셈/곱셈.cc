36042b0ef8dd0372d00518a6f61182a544541dbd
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 16:49:52 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main() {
+    int a, b;
+    
+    scanf("%d%d", &a, &b);
+    
+    printf("%d\n", a*(b%10));
+    printf("%d\n", a*((b/10)%10));
+    printf("%d\n", a*(b/100));
+    printf("%d\n", a*b);
+    
+    return 0;
+}
