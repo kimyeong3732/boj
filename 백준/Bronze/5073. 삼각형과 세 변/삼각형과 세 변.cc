f1766c6a807dbdaf75c0899331408ea90f94c6c9
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 21:13:10 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.cc
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+int main() {
+    int a, b, c;
+    
+    while(1) {
+        scanf("%d%d%d", &a, &b, &c);
+        
+        if((a==0) && (b==0) && (c==0))
+            break;
+        
+        if(((a+b)>c) && ((b+c)>a) && ((a+c)>b)) {
+            if((a==b) && (b==c))
+                printf("Equilateral\n");
+            else if((a==b) || (b==c) || (a==c))
+                printf("Isosceles\n");
+            else
+                printf("Scalene\n");
+        }
+        else
+            printf("Invalid\n");
+    }
+    
+    return 0;
+}
