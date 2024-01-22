99426962aee14c39fda04b58584da4711e875897
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 21:29:50 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main() {
+    char nick[21];
+    
+    scanf("%s", nick);
+    
+    printf(":fan::fan::fan:\n:fan::%s::fan:\n:fan::fan::fan:\n", nick);
+    
+    return 0;
+}
