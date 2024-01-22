44f5d30792eae44ed6ce7f48a225fe2ea388cb4b
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 16:49:17 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main() {
+    int n;
+    
+    scanf("%d", &n);
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n-1-i; j++) printf(" ");
+        for(int j=0; j<i+1; j++) printf("*");
+        printf("\n");
+    }
+    
+    return 0;
+}
