55b3c950b9f4f5c92945098237c9502c44bf95ac
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 16:49:14 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+    int year;
+    
+    scanf("%d", &year);
+    
+    if((year%400) == 0) printf("1\n");
+    else {
+        if(((year%4) == 0) && ((year%100) != 0)) printf("1\n");
+        else printf("0\n");
+    }
+    
+    return 0;
+}
