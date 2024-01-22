38a87507c58680dee436bbf12bec0fce3504972c
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 21:26:19 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    int result = 1;
+    
+    cin >> n;
+    
+    for(int i=0; i<n; i++)
+        result *= 2;
+    
+    cout << result << endl;
+    
+    return 0;
+}
