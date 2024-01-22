ab64de5d40c24578482840cffdc34744641b5096
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 17:05:52 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+int main() {
+    int a, b;
+    
+    scanf("%d%d", &a, &b);
+    
+    a = a%10*100 + a/10%10*10 + a/100;
+    b = b%10*100 + b/10%10*10 + b/100;
+    
+    if(a > b) printf("%d\n", a);
+    else printf("%d\n", b);
+    
+    return 0;
+}
