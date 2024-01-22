40ef41ddd519699cd7c7099acf255b3876dde7a2
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 17:06:04 +0900

    [Bronze IV] Title: 주사위 세개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.cc
@@ -0,0 +1,26 @@
+#include <cstdio>
+
+int main() {
+    int a, b, c;
+    
+    scanf("%d%d%d", &a, &b, &c);
+    
+    if((a==b) && (b==c))
+        printf("%d\n", 10000+(a*1000));
+    else if((a==b) || (b==c) || (a==c)) {
+        if((a==b) || (a==c))
+            printf("%d", 1000+(a*100));
+        else
+            printf("%d\n", 1000+(b*100));
+    }
+    else {
+        if((a>b) && (a>c))
+            printf("%d\n", a*100);
+        else if((b>a) && (b>c))
+            printf("%d\n", b*100);
+        else
+            printf("%d\n", c*100);
+    }
+    
+    return 0;
+}
