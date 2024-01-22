86504ab06ed1cdeaeebb9b9b4207e2289c4a30cd
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 20:59:02 +0900

    [Bronze V] Title: 크냐?, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/4101. 크냐？/크냐？.cc
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main() {
+    int a, b;
+    
+    scanf("%d%d", &a, &b);
+    
+    while((a!=0) || (b!=0)) {
+        if(a > b)
+            printf("Yes\n");
+        else
+            printf("No\n");
+        scanf("%d%d", &a, &b);
+    }
+    
+    return 0;
+}
