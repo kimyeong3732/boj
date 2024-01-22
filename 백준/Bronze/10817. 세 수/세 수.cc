9c73e51df42720fce69bc62dc40c0ff601aa34c8
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-22 17:09:11 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.cc
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int arr[3];
+    
+    for(int i=0; i<3; i++)
+        scanf("%d", arr+i);
+    
+    sort(arr, arr+3);
+    
+    printf("%d\n", arr[1]);
+    
+    return 0;
+}
