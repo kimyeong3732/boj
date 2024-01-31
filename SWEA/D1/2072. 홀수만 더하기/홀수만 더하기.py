078f266c5b33518709215474230b8d45f1d27e86
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-01-31 17:49:04 +0900

    [D1] Title: 홀수만 더하기, Time: 177 ms, Memory: 58,244 KB -BaekjoonHub

--- SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.py
+++ SWEA/D1/2072. 홀수만 더하기/홀수만 더하기.py
@@ -42,12 +42,13 @@ T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     # ///////////////////////////////////////////////////////////////////////////////////
+    nums = list(map(int, input().split()))
     result = 0
-    num = list(map(int, input().split()))
     
     for i in range(10):
-        if num[i] % 2 == 1:
-            result += num[i]
+        if nums[i] % 2 == 1:
+            result += nums[i]
             
-    print('#' + str(test_case) + ' ' + str(result))
+    print('#' + str(test_case), result)
+    
     # ///////////////////////////////////////////////////////////////////////////////////
