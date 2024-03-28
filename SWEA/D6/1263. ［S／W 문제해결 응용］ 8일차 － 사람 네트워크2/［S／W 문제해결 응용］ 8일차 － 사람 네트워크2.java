5a9197df7fe02078d98c3523eddf90c73eaecf97
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-03-28 12:12:35 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 2,732 ms, Memory: 106,316 KB -BaekjoonHub

---
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class Solution {
+
+	static int N;
+	static int[][] mat;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t=1; t<=T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			N = Integer.parseInt(st.nextToken());
+			mat = new int[N][N];
+			
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					int temp = Integer.parseInt(st.nextToken());
+					
+					if(temp == 0) {
+						temp = 1000;
+					}
+					mat[i][j] = temp;
+				}
+			}
+			
+			floid();
+			
+			sb.append("#").append(t).append(" ").append(find()).append("\n");
+		}
+
+		System.out.println(sb);
+	}
+	
+	static void floid() {
+		for(int i=0; i<N; i++ ) {
+			for(int j=0; j<N; j++) {
+				if(j == i) {
+					continue;
+				}
+				
+				for(int k=0; k<N; k++) {
+					if(k==i || k==j) {
+						continue;
+					}
+					
+					mat[j][k] = Math.min(mat[j][k], mat[j][i] + mat[i][k]);
+				}
+			}
+		}
+	}
+	
+	static int find() {
+		int result = 0;
+		
+		for(int i=1; i<N; i++) {
+			result += mat[0][i];
+		}
+		
+		for(int i=1; i<N; i++) {
+			int local_sum = 0;
+			
+			for(int j=0; j<N; j++) {
+				if(i != j) {
+					local_sum += mat[i][j];
+				}
+			}
+			
+			if(result > local_sum) {
+				result = local_sum;
+			}
+		}
+		
+		return result;
+	}
+}
