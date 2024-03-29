1adb7844e89140cbf683df7aac6f1c6ed5ffb938
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2024-03-29 12:16:27 +0900

    [Silver IV] Title: 図書館 2 (Library 2), Time: 44 ms, Memory: 8680 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/24448. 図書館 2 （Library 2）/図書館 2 （Library 2）.cc
+++ 백준/Silver/24448. 図書館 2 （Library 2）/図書館 2 （Library 2）.cc
@@ -5,14 +5,19 @@
 using namespace std;
 
 int main() {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(false);
     int n;
     string cmd = "READ";
+    cmd.reserve(10);
     stack<string> s;
     
     cin >> n;
     
     for(int i=0; i<n; i++) {
         string temp;
+        temp.reserve(10);
         cin >> temp;
         
         if(temp == cmd) {
