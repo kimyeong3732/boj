1fe4f95c4031237a0537bc4bb13852f8507b5957
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2025-05-30 13:31:12 +0900

    [Bronze IV] Title: 히스토그램, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/13752. 히스토그램/히스토그램.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    
+    cin >> n;
+    
+    for(int i=0; i<n; i++) {
+        int s;
+        
+        cin >> s;
+        
+        for(int j=0; j<s; j++) {
+            cout << "=";
+        }
+        
+        cout << "\n";
+    }
+    
+    return 0;
+}
