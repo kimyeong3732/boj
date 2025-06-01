068d4995ecb1a23ee5aa954f16f48f267a80d914
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2025-06-01 12:17:52 +0900

    [Bronze IV] Title: 모음의 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10987. 모음의 개수/모음의 개수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int result = 0;
+    string s;
+    
+    cin >> s;
+    
+    for(int i=0; i<s.size(); i++)
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u')
+            result++;
+    
+    cout << result;
+    
+    return 0;
+}
