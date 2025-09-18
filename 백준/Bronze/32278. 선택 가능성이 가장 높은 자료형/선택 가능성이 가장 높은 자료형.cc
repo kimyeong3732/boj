8cf4be57ee2c02051fb71654196218ce3f70a7d4
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2025-09-18 15:21:25 +0900

    [Bronze IV] Title: 선택 가능성이 가장 높은 자료형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32278. 선택 가능성이 가장 높은 자료형/선택 가능성이 가장 높은 자료형.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n;
+    
+    cin >> n;
+    
+    if(n>=-32768 && n<32768)
+        cout << "short";
+    else if(n>=-2147483648 && n<2147483648)
+        cout << "int";
+    else
+        cout << "long long";
+    
+    return 0;
+}
