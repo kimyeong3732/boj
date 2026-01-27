b39912b837206edf56dd28da0ee57d092b306508
kimyeong3732 <82482214+kimyeong3732@users.noreply.github.com>
2026-01-27 14:39:16 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 280 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+void star(int x, int y, int n) {
+    if((x/n)%3==1 && (y/n)%3==1)
+        cout << " ";
+    
+    else {
+        if(n/3 == 0)
+            cout << "*";
+        
+        else
+            star(x, y, n/3);
+    }
+}
+
+int main() {
+    int n;
+    
+    cin >> n;
+    
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++)
+            star(j, i, n);
+        
+        cout << "\n";
+    }
+    
+    return 0;
+}
